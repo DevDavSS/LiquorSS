57421fb3ff213693aca28c5a06921d8bc119a167
David Sanchez <david.guevara.fs.09@gmail.com>
2025-03-25 00:31:27 -0600

    Version 1.0

--- LiquorSS/screens/Profile.js
+++ LiquorSS/screens/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { useState, useEffect } from 'react';
 import { Wrapper} from '../components/layout/Wrapper'
 import {Content} from '../components/layout/Content'
 import { ProfImage2 } from '../components/layout/ProfImage';
@@ -8,24 +8,55 @@ import { Title } from '../components/layout/Titles';
 import Fonts from '../constants/Fonts';
 import { InfoLine } from '../components/Controls/InfoLine';
 import { ButtonClassTwo } from '../components/Controls/Buttons';
+import { auth, db } from '../firebase-config';
+import { collection } from 'firebase/firestore';
 
 
 export default function Profile() {
+
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = auth.currentUser;
+        if (!user) return;
+
+        const userRef = collection(db, "usuarios").doc(user.uid);
+        const doc = await userRef.get();
+
+        if (!doc.exists) {
+          console.log("No such document!");
+        } else {
+          setUser(doc.data());
+        }
+
+        setLoading(false);
+      } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <View>
       <Wrapper>
         <Content>
           <Title label={"Profile"}></Title>
         <ProfImage2></ProfImage2>
-        <Text style={styles.UserName}>Kimberly Wexler</Text>          
+        <Text style={styles.UserName}></Text>          
         </Content>
         <Content>
           <View style={styles.container}>
-            <InfoLine label={"User: "} information={"kim Wexler"}></InfoLine>
-            <InfoLine label={"Number: "}  information={"558293844"}></InfoLine>
+            <InfoLine label={"User: "  } information={"doc.full_name"}></InfoLine>
+
             <InfoLine label={"Email: "}  information={"SaulGdMan@gmail.com"}></InfoLine>
             <InfoLine label={"Birth Date: "}  information={"12/12/1990"}></InfoLine>
-            <InfoLine label={"Password: "}  information={"123234"}></InfoLine>            
+        
           </View>
           <View  style={styles.editButton}>
             <ButtonClassTwo label={"Edit Infomation"}>
